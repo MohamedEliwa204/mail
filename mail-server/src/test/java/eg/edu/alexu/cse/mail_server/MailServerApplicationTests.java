04e1cfdc91b5bdf349864f70af319b2cd85278b7
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-12-03 10:48:42 +0200

    first commit

---
+++ mail-server/src/test/java/eg/edu/alexu/cse/mail_server/MailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.cse.mail_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
